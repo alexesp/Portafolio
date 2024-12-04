c963d841ccac8545527c2e05a0003f6b6a899f7a
alexesp <alexizaes@hotmail.com>
2024-12-05 00:07:59 +0100

    fist

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -8,15 +8,47 @@ namespace Portafolio.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        private readonly IRepositorioProyectos repositorioProyectos;
+        //private readonly ServicioDelimitado servicioDelimitado;
+        //private readonly ServicioUnico servicioUnico;
+        //private readonly ServicioTransitorio servicioTransitorio;
+        //public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos,
+        //    ServicioDelimitado servicioDelimitado, ServicioUnico servicioUnico,
+        //    ServicioTransitorio servicioTransitorio
+        //    )
+        //{
+        //    _logger = logger;
+        //    this.repositorioProyectos = repositorioProyectos;
+        //    this.servicioDelimitado = servicioDelimitado;
+        //    this.servicioUnico = servicioUnico;
+        //    this.servicioTransitorio = servicioTransitorio;
+        //}
+        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos)
+        {
+            _logger = logger;
+            this.repositorioProyectos = repositorioProyectos;
+        }
         public IActionResult Index()
         {
-            var repositorioProyectos = new RepositorioProyectos();
+            //var repositorioProyectos = new RepositorioProyectos();
             var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
-            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+            //var guidViewModel = new EjemploGUIDViewModel()
+            //{
+            //    Delimitado = servicioDelimitado.ObtenerGuid,
+            //    Transitorio = servicioTransitorio.ObtenerGuid,
+            //    Unico = servicioUnico.ObtenerGuid
+            //};
+            //var modelo = new HomeIndexViewModel() {
+            //    Proyectos = proyectos,
+            //EjemploGUID_1 = guidViewModel};
+            var modelo = new HomeIndexViewModel()
+            {
+                Proyectos = proyectos
+
+            };
             return View(modelo);
         }
-       
+
 
         public IActionResult Privacy()
         {
