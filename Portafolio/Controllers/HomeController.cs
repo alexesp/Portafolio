cd38e0230cc9e22caedebc53c059819acb3e2abc
alexesp <alexizaes@hotmail.com>
2024-12-04 11:18:40 +0100

    Agregar archivos de proyecto.

---
+++ Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using Portafolio.Servicios;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public IActionResult Index()
+        {
+            var repositorioProyectos = new RepositorioProyectos();
+            var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+            return View(modelo);
+        }
+       
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,38 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public class RepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto {
+                    Titulo = "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP.NET Core",
+                    Link = "https://aws.amazon.com",
+                    ImageURL = "/imagenes/aws_logo.png"
+                },
+                 new Proyecto {
+                    Titulo = "Microsoft",
+                    Descripcion = "E-Commerce realizado en ASP.NET MVC",
+                    Link = "https://macrosoft.com",
+                    ImageURL = "/imagenes/azure_logo.png"
+                },
+                  new Proyecto {
+                    Titulo = "Oracle",
+                    Descripcion = "E-Commerce realizado en SQL",
+                    Link = "https://oracle.com",
+                    ImageURL = "/imagenes/oracle_logo.png"
+                },
+                   new Proyecto {
+                    Titulo = "Google",
+                    Descripcion = "E-Commerce realizado en Golang",
+                    Link = "https://google.com",
+                    ImageURL = "/imagenes/google_logo.png"
+                }
+            };
+        }
+    }
+}
